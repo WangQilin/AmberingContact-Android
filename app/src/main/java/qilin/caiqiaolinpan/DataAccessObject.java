9c9d67bf1ebc96497813576e4a41f5c34cac413a
Wang, Qilin <qilwang@paypal.com>
2015-03-23 14:28:34 +0800

    [WIP] DAO

--- app/src/main/java/qilin/caiqiaolinpan/DataAccessObject.java
+++ app/src/main/java/qilin/caiqiaolinpan/DataAccessObject.java
@@ -1,6 +1,8 @@
 package qilin.caiqiaolinpan;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
 import java.sql.SQLException;
@@ -29,5 +31,23 @@ public class DataAccessObject {
 
     public void close() {
         dbHelper.close();
+    }
+
+    public DatabaseModel createDatabaseModel(String name, String phone, String dob, String dateAdded) {
+        ContentValues values = new ContentValues();
+
+        values.put(dbHelper.COLUMN_NAME, name);
+        values.put(dbHelper.COLUMN_PHONE, phone);
+        values.put(dbHelper.COLUMN_DOB, dob);
+        values.put(dbHelper.COLUMN_DATE_ADDED, dateAdded);
+
+        long insertId = db.insert(dbHelper.TABLE_NAME, null, values);
+
+        Cursor cursor = db.query(dbHelper.TABLE_NAME, allColumns, dbHelper.COLUMN_UID + " = " + insertId, null, null, null, null);
+        cursor.moveToFirst();
+        //todo
+        DatabaseModel dbModel = null;
+
+        return dbModel;
     }
 }
