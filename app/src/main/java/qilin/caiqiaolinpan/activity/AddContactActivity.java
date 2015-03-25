f21b3ccc2cb24f968429c5f99903f18e47fa0a28
Wang Qilin <wangqilin129@gmail.com>
2015-03-25 22:31:29 +0800

    add contact, date picker added

--- app/src/main/java/qilin/caiqiaolinpan/activity/AddContactActivity.java
+++ app/src/main/java/qilin/caiqiaolinpan/activity/AddContactActivity.java
@@ -1,19 +1,52 @@
 package qilin.caiqiaolinpan.activity;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.util.Calendar;
 
 import qilin.caiqiaolinpan.R;
 
 public class AddContactActivity extends ActionBarActivity {
 
+    private String name;
+    private String phone;
+    private String dob;
+    private String dateAdded;
+
+    private EditText et_new_contact_name;
+    private EditText et_new_contact_phone;
+    private EditText et_new_contact_dob;
+
+    private Calendar c;
+
+    private final static int DATE_DIALOG = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_contact);
+
+        et_new_contact_name = (EditText) findViewById(R.id.et_new_contact_name);
+        et_new_contact_phone = (EditText) findViewById(R.id.et_new_contact_phone);
+        et_new_contact_dob = (EditText) findViewById(R.id.et_new_contact_dob);
+
+        // onClickListener will only respond when user presses twice
+        et_new_contact_dob.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                showDialog(DATE_DIALOG);
+                return false;
+            }
+        });
     }
 
 
@@ -26,9 +59,6 @@ public class AddContactActivity extends ActionBarActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
@@ -42,4 +72,26 @@ public class AddContactActivity extends ActionBarActivity {
     public void addContact(View v) {
 
     }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        Dialog dialog = null;
+        switch (id) {
+            case DATE_DIALOG:
+                c = Calendar.getInstance();
+                dialog = new DatePickerDialog(
+                        this, new DatePickerDialog.OnDateSetListener() {
+                    public void onDateSet(DatePicker dp, int year, int month, int dayOfMonth) {
+                        et_new_contact_dob.setText(year + "-" + (month + 1) + "-" + dayOfMonth);
+                    }
+                },
+                        c.get(Calendar.YEAR),
+                        c.get(Calendar.MONTH),
+                        c.get(Calendar.DAY_OF_MONTH)
+                );
+                break;
+        }
+        return dialog;
+    }
+
 }
